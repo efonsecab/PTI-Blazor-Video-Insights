acaca50ddadabf07e617b17b4beac40cd5cb6a07
Eduardo Fonseca <eduardo.fonseca@pticostarica.com>
2020-08-16 09:47:01 -0600

    Adding base for automated testing

---
+++ PTIBlazorVideoInsightsCourseSln/PTIBlazorVideoInsightsCourse.AutomatedTests/Server/Configuration/AzureConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PTIBlazorVideoInsightsCourse.AutomatedTests.Server.Configuration
+{
+    public class AzureConfiguration
+    {
+        public VideoIndexerConfiguration VideoIndexerConfiguration { get; set; }
+    }
+
+    public class VideoIndexerConfiguration
+    {
+        public string BaseAPIUrl { get; set; }
+        public string SubscriptionKey { get; set; }
+    }
+}
---
+++ PTIBlazorVideoInsightsCourseSln/PTIBlazorVideoInsightsCourse.AutomatedTests/Server/VideoIndexerControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PTIBlazorVideoInsightsCourse.AutomatedTests.Server
+{
+    [TestClass]
+    public class VideoIndexerControllerTests
+    {
+        private Server.Configuration.VideoIndexerConfiguration VideoIndexerConfiguration { get; set; }
+        [TestInitialize]
+        public void InitializeTests()
+        {
+            this.VideoIndexerConfiguration =
+                new Configuration.VideoIndexerConfiguration()
+                {
+                    BaseAPIUrl="",
+                    SubscriptionKey = ""
+                };
+        }
+
+        [TestMethod]
+        public void ListVideosTest()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void GetAccountAccessToken()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
