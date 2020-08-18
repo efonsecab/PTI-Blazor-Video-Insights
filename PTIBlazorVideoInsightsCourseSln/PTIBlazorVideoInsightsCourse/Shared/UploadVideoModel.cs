914874191233203e11a3fa67be2249f6d465b6e2
Eduardo Fonseca <efonsecab@gmail.com>
2020-08-17 20:20:43 -0600

    Adding localization to Data Annotations

--- PTIBlazorVideoInsightsCourseSln/PTIBlazorVideoInsightsCourse/Shared/UploadVideoModel.cs
+++ PTIBlazorVideoInsightsCourseSln/PTIBlazorVideoInsightsCourse/Shared/UploadVideoModel.cs
@@ -5,11 +5,14 @@ namespace PTIBlazorVideoInsightsCourse.Shared
 {
     public class UploadVideoModel
     {
-        [Required]
+        [Required(ErrorMessageResourceName ="RequiredName",
+            ErrorMessageResourceType =typeof(Resources.UploadVideoModel))]
         public string Name { get; set; }
         public string CallbackUrl { get; set; }
-        [Required]
-        [Url]
+        [Required(ErrorMessageResourceName = "RequiredVideoUrl",
+            ErrorMessageResourceType = typeof(Resources.UploadVideoModel))]
+        [Url(ErrorMessageResourceName = "InvalidVideoUrlFormat",
+            ErrorMessageResourceType = typeof(Resources.UploadVideoModel))]
         public string VideoUrl { get; set; }
         public bool SendSuccessEmail { get; set; }
     }
