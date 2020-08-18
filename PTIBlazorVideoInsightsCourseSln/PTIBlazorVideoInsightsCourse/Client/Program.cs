c0dd37882c26d370872e6d3ec2d50410a458f0e6
Eduardo Fonseca <efonsecab@gmail.com>
2020-08-17 19:03:03 -0600

    Adding support for localization

--- PTIBlazorVideoInsightsCourseSln/PTIBlazorVideoInsightsCourse/Client/Program.cs
+++ PTIBlazorVideoInsightsCourseSln/PTIBlazorVideoInsightsCourse/Client/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System.Globalization;
+using Microsoft.JSInterop;
 
 namespace PTIBlazorVideoInsightsCourse.Client
 {
@@ -17,6 +19,22 @@ namespace PTIBlazorVideoInsightsCourse.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddLocalization();
+            var host = builder.Build();
+            var jsInterop = host.Services.GetRequiredService<IJSRuntime>();
+            var result = await jsInterop.InvokeAsync<string>("blazorCulture.get");
+            if (result != null)
+            {
+                var culture = new CultureInfo(result);
+                CultureInfo.DefaultThreadCurrentCulture = culture;
+                CultureInfo.DefaultThreadCurrentUICulture = culture;
+            }
+            else
+            {
+                CultureInfo.DefaultThreadCurrentCulture = CultureInfo.CurrentUICulture =
+                    CultureInfo.GetCultureInfo("en");
+            }
+
 
             await builder.Build().RunAsync();
         }
--- PTIBlazorVideoInsightsCourseSln/PTIBlazorVideoInsightsCourse/Shared/UploadVideoModel.cs
+++ PTIBlazorVideoInsightsCourseSln/PTIBlazorVideoInsightsCourse/Shared/UploadVideoModel.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace PTIBlazorVideoInsightsCourse.Shared
 {
     public class UploadVideoModel
     {
+        [Required]
         public string Name { get; set; }
         public string CallbackUrl { get; set; }
+        [Required]
+        [Url]
         public string VideoUrl { get; set; }
         public bool SendSuccessEmail { get; set; }
     }
