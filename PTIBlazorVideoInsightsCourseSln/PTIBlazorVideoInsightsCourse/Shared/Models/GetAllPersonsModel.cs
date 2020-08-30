d8f06ca120afaf024fdb40e66ce7bec99cc918e3
Eduardo Fonseca <efonsecab@gmail.com>
2020-08-30 17:05:06 -0600

    Adding ability to list all persons in account and search their associated videos.

---
+++ PTIBlazorVideoInsightsCourseSln/PTIBlazorVideoInsightsCourse/Shared/Models/GetAllPersonsModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PTIBlazorVideoInsightsCourse.Shared.Models
+{
+    public class GetAllPersonsModel
+    {
+        public List<GettAllPersonsModelItem> Persons { get; set; }
+    }
+
+    public class GettAllPersonsModelItem
+    {
+        public string PersonName { get; set; }
+        public string PersonPictureBase64 { get; set; }
+    }
+}
