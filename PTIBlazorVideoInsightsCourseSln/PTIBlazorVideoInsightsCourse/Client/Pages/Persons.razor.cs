0e8232491cc393a9b3af56ae4bef1fe0b40c7dc9
Eduardo Fonseca <efonsecab@gmail.com>
2020-09-06 11:20:35 -0600

    Adding missing files

---
+++ PTIBlazorVideoInsightsCourseSln/PTIBlazorVideoInsightsCourse/Client/Pages/Persons.razor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using PTIBlazorVideoInsightsCourse.Shared.Models;
+using PTIBlazorVideoInsightsCourse.Shared.Models.AzureVideoIndexer.GetPersons;
+
+namespace PTIBlazorVideoInsightsCourse.Client.Pages
+{
+    public partial class Persons
+    {
+        [Parameter]
+        public string PersonModelId { get; set; }
+        private bool IsLoading { get; set; }
+        private GetPersonsResponse PersonsResult { get; set; }
+        private GetAllPersonsModel GetAllPersonsResult { get; set; }
+        [Inject]
+        private HttpClient httpClient { get; set; }
+        private Dictionary<string, string> PersonsPictures { get; set; } = new Dictionary<string, string>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                this.IsLoading = true;
+                if (this.PersonModelId != null)
+                {
+                    var tmpPersonsResult = await httpClient.GetFromJsonAsync<GetPersonsResponse>
+                        ($"VideoIndexer/GetPersons?personModelId={this.PersonModelId}");
+                    foreach (var singlePerson in tmpPersonsResult.results)
+                    {
+                        string base64Image = await httpClient.GetStringAsync($"VideoIndexer" +
+                            $"/GetPersonPicture?personModelId={this.PersonModelId}" +
+                            $"&personId={singlePerson.id}" +
+                            $"&faceId={singlePerson.sampleFace.id}");
+                        string base64ImgSrcString = String.Format("data:image/gif;base64,{0}", base64Image);
+                        this.PersonsPictures.Add(singlePerson.id, base64ImgSrcString);
+                    }
+                    this.PersonsResult = tmpPersonsResult;
+                }
+                else
+                {
+                    this.GetAllPersonsResult =
+                        await httpClient.GetFromJsonAsync<GetAllPersonsModel>("VideoIndexer/GetAllPersons");
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                this.IsLoading = false;
+            }
+        }
+    }
+}
---
+++ PTIBlazorVideoInsightsCourseSln/PTIBlazorVideoInsightsCourse/Client/Pages/PersonsModels.razor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using PTIBlazorVideoInsightsCourse.Shared.Models.AzureVideoIndexer.GetPersonModels;
+
+namespace PTIBlazorVideoInsightsCourse.Client.Pages
+{
+    public partial class PersonsModels
+    {
+        private bool IsLoading { get;  set; }
+        private PersonModel[] PersonsModelsResult { get; set; }
+        [Inject]
+        private HttpClient httpClient { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                this.IsLoading = true;
+                this.PersonsModelsResult = await httpClient.GetFromJsonAsync<PersonModel[]>
+                    ("VideoIndexer/GetPersonsModels");
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                this.IsLoading = false;
+            }
+        }
+    }
+}
