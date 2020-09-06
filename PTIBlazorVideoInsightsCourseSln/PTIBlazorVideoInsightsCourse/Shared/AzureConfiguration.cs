ad208d9510df591e60ac8279102242f52fb3e544
Eduardo Fonseca <efonsecab@gmail.com>
2020-09-06 11:12:34 -0600

    adding missing files

---
+++ PTIBlazorVideoInsightsCourseSln/PTIBlazorVideoInsightsCourse/Shared/AzureConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PTIBlazorVideoInsightsCourse.Shared
+{
+    public class AzureConfiguration
+    {
+        public VideoIndexerConfiguration VideoIndexerConfiguration { get; set; }
+    }
+
+    public class VideoIndexerConfiguration
+    {
+        public string BaseAPIUrl { get; set; }
+        public string SubscriptionKey { get; set; }
+        public string Location { get; set; }
+        public string AccountId { get; set; }
+    }
+}
